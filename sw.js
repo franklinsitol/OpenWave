c9884ff6afda48704afc61254d54e6f4319cc3a8
franklinsitol <franklin.toledo@gmail.com>
2024-10-22 14:45:42 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,21 @@
-const cacheName = 'social-network-pwa-v1';
-const filesToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
-];
-
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(cacheName)
-      .then((cache) => {
-        return cache.addAll(filesToCache);
-      })
+    caches.open('openwave-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/scripts.js',
+        '/manifest.json'
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
 });
